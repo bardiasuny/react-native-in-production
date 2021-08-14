e578ddf8dee9ab173487750072520795685d2edf
Bardia <bardiasuny@gmail.com>
2021-08-14 18:13:43 +0100

    new logo and text logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import sponsors from './assets/sponsors.png';
-import textLogo from './assets/textLogo.png';
-import reactLogo from './assets/reactLogo.png';
-
-
 
 import './App.css';
 import  Box  from './Components/Layout/Box';
+import sponsors from './assets/sponsors.png';
+import textLogo from './assets/textLogo.png';
+import reactLogo from './assets/reactLogo.png';
 import YouTube from 'react-youtube';
 
 function App() {
@@ -15,14 +13,19 @@ function App() {
     width: '310',
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
+      autoplay: 0,
     },
   };
+
+ const onReady = (event) => {
+    // access to player in all event handlers via event.target
+   // event.target.pauseVideo();
+  }
 
   return (
     <div >
       <header className="App-header">
-        <Box mt='3vh' mb='3vh'  style={styles.test} vCenter justifyContent="space-between"  height='92vh' width='100vw'>
+        <Box mt='2vh'  style={styles.test} vCenter justifyContent="space-between"  height='97vh' width='100vw'>
            <Box center>
               <img src={textLogo} style={styles.textLogo} alt="banner" />
               <Box mt={2} fontSize={12}>video tutorials about React Native</Box>
@@ -32,16 +35,19 @@ function App() {
           
                <img src={reactLogo} style={styles.reactLogo} alt="banner" />
              
-               <Box fontSize={25} fontWeight='800' mt='3vh'>COMING SOON</Box>
-               <Box mt={20}>
+               <Box fontSize={25}  fontWeight='800' mt='3vh'>COMING SOON</Box>
+               {/* <Box mt={20}>
                   <YouTube 
-                      videoId="PyWOFi5eUgU" 
+                      videoId="GFAqaoSIAhg" 
                       opts={opts} 
-                      // onReady={onReady} 
+                      onReady={onReady} 
                   />
-              </Box>
+              </Box> */}
            </Box>
-           <Box row center justifyContent="space-around" width="90vw">
+           <Box 
+           pt={5}
+           style={styles.footerWrapper}
+           row center justifyContent="space-around" width="90vw">
                 <Box  fontSize={12}>©2021 React Native in Production UK. All rights reserved.</Box>
                 <img src={sponsors} style={styles.sponsorsLogo} alt="banner" />
             </Box>
@@ -69,6 +75,11 @@ const styles = {
   },
   reactLogo: {
      height: '20vh'
+  },
+  footerWrapper:{
+     borderTopWidth: '1px',
+     borderTopColor: 'grey',
+     borderTopStyle: 'solid'
   }
 }
 
